03742cea01b00fca94036d60e1f86735f25c876e
jefferson <bonnaire.jefferson@gmail.com>
2012-08-05 15:51:07 +0200

    Suppresion des liens google

--- includes/footer.php
+++ includes/footer.php
@@ -3,39 +3,39 @@
 	<div class="five columns centered hide-on-mobile">
 		<div class="fb-like" data-href="http://www.showtime2012.fr" data-send="true" data-width="450" data-show-faces="true" data-action="recommend" data-colorscheme="dark" data-font="arial"></div>
 	</div>
-    <p class="text-center copyright"> COPYRIGHT &copy; ShowTime BDE TELECOM Ecole de Management & TELECOM SudParis</p>	
+    <p class="text-center copyright"> COPYRIGHT & ShowTime BDE TELECOM Ecole de Management & TELECOM SudParis</p>	
 </footer>
 
 <div id="myModal" class="reveal-modal">
-<div class="row">
-	<div class="twelve columns">
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">m</span></p>
-			<p class="text-center"> <a class="gaspar" href="http://gaspar.it-sudparis.eu/render.userLayoutRootNode.uP" title="Accède à ce dont tu as besoin tout au long de l'année !">Gaspar</a></p>
-		</div>
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">h</span></p>
-			<p class="text-center"> <a class="zimbra" href="https://z.mines-telecom.fr/" title="Consulte tes mails avec Zimbra !">Zimbra</a></p>
-		</div>
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">0</span></p>
-			<p class="text-center"> <a class="trombi" href="https://trombi.it-sudparis.eu/index.php" title="C'est qui lui/elle ?">Trombi</a></p>
-		</div>
-	</div><!-- fin de twelve -->
-	
-	<div class="twelve columns">
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">6</span></p>
-			<p class="text-center"> <a class="ratp" href="http://www.ratp.fr/" title="Consulte les horaires des RER !">RATP</a></p>
-		</div>
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">7</span></p>
-			<p class="text-center"> <a class="sncf" href="http://www.sncf.fr/" title="Prépare ton voyage !">SNCF</a></p>
-		</div>
-		<div class="four columns">
-			<p class="text-center"><span class="glyph general">i</span></p>
-			<p class="text-center"> <a class="calendrier" href="calendrier.php" title="Tout les événements de ton campus !">Calendrier</a></p>
-		</div>
-	</div><!-- fin de twelve -->
-</div>
-</div>
+	<div class="row">
+		<div class="twelve columns">
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">m</span></p>
+				<p class="text-center"> <a class="gaspar" href="http://gaspar.it-sudparis.eu/render.userLayoutRootNode.uP" title="Accède à ce dont tu as besoin tout au long de l'année !">Gaspar</a></p>
+			</div>
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">h</span></p>
+				<p class="text-center"> <a class="zimbra" href="https://z.mines-telecom.fr/" title="Consulte tes mails avec Zimbra !">Zimbra</a></p>
+			</div>
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">0</span></p>
+				<p class="text-center"> <a class="trombi" href="https://trombi.it-sudparis.eu/index.php" title="C'est qui lui/elle ?">Trombi</a></p>
+			</div>
+		</div><!-- fin de twelve -->
+		
+		<div class="twelve columns">
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">6</span></p>
+				<p class="text-center"> <a class="ratp" href="http://www.ratp.fr/" title="Consulte les horaires des RER !">RATP</a></p>
+			</div>
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">7</span></p>
+				<p class="text-center"> <a class="sncf" href="http://www.sncf.fr/" title="Prépare ton voyage !">SNCF</a></p>
+			</div>
+			<div class="four columns">
+				<p class="text-center"><span class="glyph general">i</span></p>
+				<p class="text-center"> <a class="calendrier" href="calendrier.php" title="Tout les événements de ton campus !">Calendrier</a></p>
+			</div>
+		</div><!-- fin de twelve -->
+	</div>
+</div><!-- fin de modal -->
