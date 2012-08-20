0c033de5fd9da4fd085f3753e769b78ea18a6982
jefferson <bonnaire.jefferson@gmail.com>
2012-08-20 17:06:19 +0200

    Ajout du fichier de navigation et correction des liens

---
+++ includes/navigation.php
@@ -0,0 +1,23 @@
+<nav class="twelve columns">
+	<a href="index.php">
+			<p class="left mobile-four"><img id="logo" src="images/logo.png" alt="Logo Showtime"/>
+	</a>
+	<div class="nine columns">
+		<ul class="nav-bar">
+		  <li class="active"><a href="actualite.php">Actualit&eacute;s</a></li>
+		  <li><a href="showtime.php">Showtime</a></li>
+		  <li><a href="partenaires.php">Partenaires</a></li>
+		  <li><a href="http://associations.it-sudparis.eu/">Associations</a></li>
+		  <li><a href="pougnes.php">Pougnes</a></li>
+		  <li class="has-flyout">
+		    <a href="#">Bonus</a>
+		    <a href="#" class="flyout-toggle"><span></span></a>
+		    <ul class="flyout">
+		      <li><a href="bonus-videos.php">Vid&eacute;os</a></li>
+		      <li><a href="bonus-photos.php">Photos</a></li>
+		      <li><a href="bonus-goodies.php">Goodies</a></li>
+		    </ul>
+		  </li>
+		</ul>
+	</div>
+</nav>
