b3efb89111332234cb763d9cb6b0bfc97159c004
jefferson <bonnaire.jefferson@gmail.com>
2012-08-28 21:26:35 +0200

    Correction du bugs des icônes et ajouts de icônes

--- includes/footer.php
+++ includes/footer.php
@@ -6,27 +6,27 @@
 	<div class="row">
 		<div class="twelve columns three-up">
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">m</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-website"></i></p>
 				<p class="text-center"> <a class="gaspar" href="http://gaspar.it-sudparis.eu/render.userLayoutRootNode.uP" title="Accède à ce dont tu as besoin tout au long de l'année !">Gaspar</a></p>
 			</div>
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">h</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-mail"></i></p>
 				<p class="text-center"> <a class="zimbra" href="https://z.mines-telecom.fr/" title="Consulte tes mails avec Zimbra !">Zimbra</a></p>
 			</div>
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">0</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-address-book"></i></p>
 				<p class="text-center"> <a class="trombi" href="https://trombi.it-sudparis.eu/index.php" title="C'est qui lui/elle ?">Trombi</a></p>
 			</div>
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">6</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-clock"></i></p>
 				<p class="text-center"> <a class="ratp" href="http://www.ratp.fr/" title="Consulte les horaires des RER !">RATP</a></p>
 			</div>
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">7</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-globe"></i></p>
 				<p class="text-center"> <a class="sncf" href="http://www.sncf.fr/" title="Prépare ton voyage !">SNCF</a></p>
 			</div>
 			<div class="four columns mobile-two">
-				<p class="text-center"><span class="glyph general">i</span></p>
+				<p class="text-center no-margin"><i class="extra-large-icons foundicon-calendar"></i></p>
 				<p class="text-center"> <a class="calendrier" href="calendrier.php" title="Tout les événements de ton campus !">Calendrier</a></p>
 			</div>
 		</div><!-- fin de twelve -->
--- includes/head.php
+++ includes/head.php
@@ -2,12 +2,12 @@
     <div class="row">
         <div class="two columns mobile-two">
             <a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal">
-                <p class="text-left LienHead">Liens utiles</p>
+                <p class="text-left LienHead">Liens utiles <i class="foundicon-plus"></i></p>
             </a>
         </div>
         <div class="two columns mobile-two">
             <a href="mailto:contact@showtime2012.fr?subject=Contact site showtime2012">
-                <p class="text-right LienHead"><i class="foundicon-mail"></i>Contact</p>
+                <p class="text-right LienHead"><i class="foundicon-mail"></i> Contact</p>
             </a>
         </div>
     </div><!-- fin de row -->
